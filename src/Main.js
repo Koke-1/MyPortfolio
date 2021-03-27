93bd5d5fba6a3afcf0d7be0002f31560c75bd115
Koke-1 <sanjuangeorge@gmail.com>
2021-03-27 12:07:53 -0500

    Testing

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React,{useEffect, useState} from 'react'
+import './css/index.css'
+
+export default function Main() {
+    const [Time, setTime] = useState()
+    
+
+ return (
+    <main>
+        <h1>
+            Hellogit
+        </h1>
+        
+    </main>
+ )
+}
+    
